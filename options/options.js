4cbe831bddaf383d64a0c274f654f8a979580d1a
Crowdbotics <team@crowdbotics.com>
2022-07-23 18:12:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "constance_glow_stor_36373",
+  url: "https://constance_glow_stor_36373.botics.co",
+  api: "https://constance_glow_stor_36373.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
